30b0cf0e6aae1a4a0d9de447e0262625e1f09a0e
Matthew Li <matthewli@MacBookPro.local>
2018-05-29 14:30:52 -0700

    recycler view with intent creation working

--- app/src/main/java/edu/piedpiper/uw/ischool/watsonchat/Thread.kt
+++ app/src/main/java/edu/piedpiper/uw/ischool/watsonchat/Thread.kt
@@ -1,4 +1,17 @@
 package edu.piedpiper.uw.ischool.watsonchat
 
+import com.google.firebase.database.IgnoreExtraProperties
+
 class Thread {
+    var timeCreated: Long? = null
+    var threadName: String? = null
+    var threadId: String? = null
+
+    constructor() {}  // Needed for Firebase
+
+    constructor(timeCreated:Long, threadName:String, threadId:String) {
+        this.timeCreated = timeCreated
+        this.threadName = threadName
+        this.threadId = threadId
+    }
 }
--- app/src/main/java/edu/piedpiper/uw/ischool/watsonchat/ThreadActivity.kt
+++ app/src/main/java/edu/piedpiper/uw/ischool/watsonchat/ThreadActivity.kt
@@ -1,9 +1,11 @@
 package edu.piedpiper.uw.ischool.watsonchat
 
+import android.app.FragmentBreadCrumbs
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
+import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
@@ -15,13 +17,15 @@ import android.widget.TextView
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ServerValue
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_thread.*
 import kotlinx.android.synthetic.main.thread_row.view.*
+import java.text.DateFormat.getTimeInstance
+import java.util.*
 
 class ThreadActivity : AppCompatActivity() {
+
+    private var mThreads: ArrayList<Thread>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,9 +37,57 @@ class ThreadActivity : AppCompatActivity() {
 //            startActivity(Intent(this, MainActivity::class.java))
 //        }
 
+        mThreads = arrayListOf()
+
+        val myAdapter = ThreadAdapter(mThreads!!)
+        recyclerView_thread.layoutManager = LinearLayoutManager(this)
+        recyclerView_thread.adapter = myAdapter
+
 
         val reference: DatabaseReference = FirebaseDatabase.getInstance()
                 .getReference("/threads")
+
+
+
+        reference.addChildEventListener(object : ChildEventListener {
+            override fun onChildMoved(p0: DataSnapshot?, p1: String?) {
+
+            }
+
+            override fun onChildChanged(p0: DataSnapshot?, p1: String?) {
+
+            }
+
+            override fun onChildRemoved(p0: DataSnapshot?) {
+
+            }
+
+            override fun onCancelled(p0: DatabaseError?) {
+
+            }
+
+            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                //val model = dataSnapshot.getValue(Thread::class.java)
+
+                Log.i("thread", dataSnapshot.child("timeCreated").toString())
+                Log.i("thread", dataSnapshot.child("timeCreated").toString())
+
+                val time = dataSnapshot.child("timeCreated").value as Long
+                val name = dataSnapshot.child("threadName").value as String
+                val id = dataSnapshot.key
+
+                val thread = Thread(threadName = name, timeCreated = time, threadId = id)
+
+
+                mThreads!!.add(thread);
+
+                myAdapter.notifyDataSetChanged()
+                //mMessageRecyclerView.smoothScrollToPosition(mChats!!.size -1);
+
+                //Log.i("MessageActivity", "" + mChats!!.size )
+                //Log.i("MessageActivity", "" + mMessageRecyclerView.childCount )
+            }
+        })
 
 
         val btn = findViewById<FloatingActionButton>(R.id.btn_action) as FloatingActionButton
@@ -47,8 +99,7 @@ class ThreadActivity : AppCompatActivity() {
             //var userId = FirebaseAuth.getInstance().currentUser!!.uid
 
             temp.put("timeCreated", ServerValue.TIMESTAMP)
-            temp.put("userIds", listOf(FirebaseAuth.getInstance().uid))
-            temp.put("userNames", listOf(FirebaseAuth.getInstance().currentUser!!.displayName))
+            temp.put("threadName", "Temp Thread Name")
 
             val key = reference.push().key
             reference.child(key).setValue(temp)
@@ -59,17 +110,20 @@ class ThreadActivity : AppCompatActivity() {
                         Log.i("MessageActivity", "Failure")
                     })
         })
+    }
 
-        recyclerView_thread.layoutManager = LinearLayoutManager(this)
-        recyclerView_thread.adapter = ThreadAdapter()
+    private fun threadClicked(thread : Thread) {
+        val messageIntent = Intent(this, ThreadActivity::class.java)
+        messageIntent.putExtra("threadId", thread.threadId)
+        startActivity(messageIntent)
     }
 }
 
-class ThreadAdapter: RecyclerView.Adapter<CustomViewHolder>() {
+class ThreadAdapter(var threads:MutableList<Thread>): RecyclerView.Adapter<CustomViewHolder>() {
 
     // number of items
     override fun getItemCount(): Int {
-        return 9
+        return threads.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder{
@@ -83,22 +137,38 @@ class ThreadAdapter: RecyclerView.Adapter<CustomViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        val contactName = holder.view.findViewById(R.id.contactName) as TextView
-        val lastText = holder.view.findViewById(R.id.lastText) as TextView
-        val timeStamp = holder.view.findViewById(R.id.timeStamp) as TextView
+        val thread = threads.get(position)
 
-
-        contactName.text = "hello world";
-        lastText.text = "hello";
-
-        timeStamp.text = "blah";
-
-        //holder.view.lastText.text = 'insert text'
-        //holder.view.timeStamp.text = 'insert time'
+        holder.bind(thread)
     }
 }
 
 class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view) {
 
+    fun bind(thread: Thread) {
+        val contactName = view.findViewById(R.id.contactName) as TextView
+        val lastText = view.findViewById(R.id.lastText) as TextView
+        val timeStamp = view.findViewById(R.id.timeStamp) as TextView
+
+        view.setOnClickListener {
+            val messageIntent = Intent(view.context, ThreadActivity::class.java)
+            messageIntent.putExtra("threadId", thread.threadId)
+            view.context.startActivity(messageIntent)
+        }
+
+        contactName.text = thread.threadName;
+        lastText.text = "temp text";
+        timeStamp.text = getTimeDate(thread.timeCreated!!)
+    }
+
+    private fun getTimeDate(timeStamp: Long): String {
+        try {
+            val dateFormat = getTimeInstance()
+            val netDate = Date(timeStamp)
+            return dateFormat.format(netDate)
+        } catch (e: Exception) {
+            return "date"
+        }
+    }
 }
 
