66089dd0e7757a2e884a8b16a93c86cbde0e5447
Eric Peng <ericpeng@uw.edu>
2018-06-05 20:35:10 -0700

    Added checking offline

--- app/src/main/java/edu/piedpiper/uw/ischool/watsonchat/MainActivity.kt
+++ app/src/main/java/edu/piedpiper/uw/ischool/watsonchat/MainActivity.kt
@@ -2,10 +2,13 @@ package edu.piedpiper.uw.ischool.watsonchat
 import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.app.LoaderManager.LoaderCallbacks
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 
 import android.database.Cursor
+import android.net.ConnectivityManager
 import com.firebase.ui.auth.AuthUI
 import java.util.*
 import java.util.Arrays.asList
@@ -19,6 +22,7 @@ import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import android.os.Build
+import android.provider.Settings
 
 
 /**
@@ -34,6 +38,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        airplaneMode()
+        checkConnection()
 
         val providers = Arrays.asList(
                 AuthUI.IdpConfig.EmailBuilder().build(),
@@ -91,6 +98,70 @@ class MainActivity : AppCompatActivity() {
      */
     protected fun overridePendingTransitionEnter() {
         overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        airplaneMode()
+        checkConnection()
+    }
+
+    @Suppress("DEPRECATED_IDENTITY_EQUALS")
+    fun airplaneMode() {
+        if (Settings.Global.getInt(this.contentResolver,
+                        Settings.Global.AIRPLANE_MODE_ON, 0) !== 0) {
+            val dialog = AlertDialog.Builder(this)
+
+            dialog.setTitle("Airplane Mode")
+            dialog.setMessage("You currently have airplane mode on so features such as chat" +
+                    " and Watson personality analysis might not work as expected. Do you want to head over to settings" +
+                    " to turn it off?")
+
+            dialog.setPositiveButton("Yes") { _, _ ->
+                val intent = Intent(android.provider.Settings.ACTION_AIRPLANE_MODE_SETTINGS)
+                startActivity(intent)
+
+            }
+
+            dialog.setNegativeButton("No") { _, _ ->
+
+            }
+
+            val d: AlertDialog = dialog.create()
+
+            d.show()
+        }
+    }
+
+    private fun checkConnection() {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+
+        if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+            val dialog = AlertDialog.Builder(this)
+
+            dialog.setTitle("No Connection")
+
+            dialog.setMessage("You currently have no internet connection so features such as chat" +
+                    " and Watson personality analysis will not work as expected. Do you want to connect" +
+                    " to a wifi network?")
+
+            dialog.setPositiveButton("Yes") { _, _ ->
+                val intent = Intent(android.provider.Settings.ACTION_WIFI_SETTINGS)
+                startActivity(intent)
+
+            }
+
+            dialog.setNegativeButton("No") { _, _ ->
+
+            }
+
+
+            val d: AlertDialog = dialog.create()
+
+            d.show()
+        }
     }
 
 
